2bae9abd626313c58a15b5690046eea16861ee96
Taras Pashniak <taraspashnyak93@gmail.com>
2023-05-31 13:30:30 +0300

    DodZavd

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -1,8 +1,4 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №3. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
-#if !defined(_MSC_VER)
+﻿#if !defined(_MSC_VER)
 #define CODING_VS_CODE
 #endif
 #include <iostream>
@@ -10,339 +6,238 @@
 #if !defined(CODING_VS_CODE)
 #include <clocale>
 #endif
-// Ваші файли загловки 
-//
+#include "Lab3Example.h"
 using namespace std;
 
-/*class Romb {
-	double a, d; // side and diagonal side of the romb
-	unsigned int color;
+
+class Matrix {
 public:
-	Romb() : a(1.0), d(2.0), color(0) {}
-	Romb(double ai) : a(ai), color(0) {}
-	Romb(int ic) : a(1.0) { if (ic >= 0) color = ic; else color = 0; }
-	Romb(double a, double d, int c) {
-		this->a = a;
-		this->d = d;
-		if (c >= 0) color = c; else color = 0;
-	}
-	double getA() const
-	{
-		return a;
-	}
-	void setA(double a)
-	{
-		if (a < 0 || a > 1.e+100)
-		{
-			cout << " Error set  a \n";
-			return;
-		}
-		this->a = a;
-	}
-	double getD() const
-	{
-		return d;
-	}
-	void setD(double d)
-	{
-		if (d < 0 || d > 1.e+100)
-		{
-			cout << " Error set  D \n";
-			return;
-		}
-		this->d = d;
-	}
-	double getColor() const
-	{
-		return color;
-	}
-	void setColor(int c)
-	{
-		if (c < 0 || c > 10000)
-		{
-			cout << " Error set  color \n";
-			return;
-		}
-		this->color = c;
-	}
-	double S() {
-		return (d * d) / 2;
-	}
-	double P() {
-		return 4 * a;
-	}
-	void printInfo()
-	{
-		cout << "\n a= " << a << "\n d= " << d << "\n color = " << color;
-		cout << "  S= " << S() << " P = " << P() << endl;
-	}
+    double** matrix; // вказівник на вказівник на double
+    int rows; // кількість рядків
+    int cols; // кількість стовпців
+    int error; // стан помилки
+    static int count; // кількість об'єктів
 
-};
-void task1() 
-{
-	Romb obj;
-	obj.printInfo();
-	double in_a, in_d; int in_color;
-	cout << " Input side, diagonal and color of Romb: "; cin >> in_a >> in_d >> in_color;
-	Romb obj1(in_a), obj2(in_d), obj3(in_color), obj4(in_a, in_d, in_color);
-	obj1.printInfo();
-	obj2.printInfo();
-	obj3.printInfo();
-	obj4.printInfo();
+    // конструктор без параметрів
+    Matrix() {
+        rows = 2;
+        cols = 2;
+        matrix = new double*[rows];
+        for (int i = 0; i < rows; i++)
+            matrix[i] = new double[cols]{0.0}; // ініціалізуємо нулями
+        error = 0;
+        count++;
+    }
 
-	cout << "Test:";
-	obj.setA(-5);
-	obj.printInfo();
-	obj.setA(5);
-	obj.printInfo();
-	obj.setA(2.e100);
-	obj.printInfo();
+    // конструктор з одним параметром
+    Matrix(int n) {
+        rows = n;
+        cols = n;
+        matrix = new double*[rows];
+        for (int i = 0; i < rows; i++)
+            matrix[i] = new double[cols]{0.0}; // ініціалізуємо нулями
+        error = 0;
+        count++;
+    }
 
-	obj.setD(-5);
-	obj.printInfo();
-	obj.setD(5);
-	obj.printInfo();
-	obj.setD(2.e100);
-	obj.printInfo();
+    // конструктор із трьома параметрами
+    Matrix(int n, int m, double value = 0.0) {
+        rows = n;
+        cols = m;
+        matrix = new double*[rows];
+        for (int i = 0; i < rows; i++)
+            matrix[i] = new double[cols]{value}; // ініціалізуємо значенням value
+        error = 0;
+        count++;
+    }
 
-	obj.setColor(-10);
-	obj.printInfo();
-	obj.setColor(10);
-	obj.printInfo();
-	obj.setColor(10001);
-	obj.printInfo();
-	cout << " End testing \n";
-	cin.get();
-}*/
-/*е*/
-/*
-Створити тип даних - клас вектор, який має поля x, y та z типу float і змінну стану. У
-класі визначити
-o конструктор без параметрів(інінціалізує поля в нуль);
-o конструктор з одним параметром типу float (інінціалізує поля x, y та z
-значенням параметру);
-o конструктор з одним параметром вказівник на тип (інінціалізує поля x, y та z
-значенням масиву за вказівником, якщо вказівник NULL (nulptr) то встановити
-код помилки);
-o деструктор із виведенням інформації про стан вектора;
-o визначити функцію, яка присвоює полю x, y або z деяке значення (параметр за
-замовчуванням);
-Об’єктно – орієнтоване програмування мовою С++ Лазорик В.В.
-16
-o функцію яка одержує деякий елемент з полів x, y та z;
-o конструктор копій та операцію присвоєння; // !!!
-o визначити функції друку, додавання, віднімання, векторний добуток які
-здійснюють ці арифметичні операції з даними цього класу;
-o функцію ділення на ціле типу short(при діленні на 0 змінити стан, а ділення не
-виконувати);
-o визначити функції порівняння: більше, менше або рівно, які повертають true
-або false.
-У змінну стани встановлювати код помилки, діленні на 0, при передачі NULL (nulptr) в
-конструкторі із вказівником. Передбачити можливість підрахунку числа об'єктів даного типу.
-Написати програму тестування всіх можливостей цього класу.
-*/
-const int OK = 0;
-const int BAD_INIT = 1;
-const int BAD_DIV = 2;
+    // конструктор копій
+    Matrix(const Matrix& other) {
+        rows = other.rows;
+        cols = other.cols;
+        matrix = new double*[rows];
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = new double[cols];
+            for (int j = 0; j < cols; j++)
+                matrix[i][j] = other.matrix[i][j]; // копіюємо елементи
+        }
+        error = other.error;
+        count++;
+    }
 
-class Vec2
-{
-	float  x, y, z;
-	int state;
-	static int count;
-public:
-	Vec2() : x(0), y(0) , z(0) {
-		state = OK; count++;
-	}   // o конструктор без параметрів(інінціалізує поля в нуль);
+    // операція присвоєння
+    Matrix& operator=(const Matrix& other) {
+        if (this == &other) // перевірка на самоприсвоєння
+            return *this;
 
-	Vec2(float iv) : x(iv), y(iv), z(iv) {
-		state = OK; count++;
-	}	// o конструктор з одним параметром типу float(інінціалізує поля x, y та z значенням параметру);
+        // видалення поточної матриці
+        for (int i = 0; i < rows; i++)
+            delete[] matrix[i];
+        delete[] matrix;
 
-	Vec2(float ix, float iy, float iz);
-	Vec2(float ix, float iy) : x(ix), y(iy),z(0) {
-		state = OK; count++;
-	};
-	Vec2(float* v) {
-		if (v == nullptr) {
-			state = BAD_INIT; x = 0; y = 0; z = 0;
-		}
-		else {
-			x = v[0]; y = v[1]; z = v[2];
-			state = OK;
-		}
-		count++;
+        // копіювання елементів з іншої матриці
+        rows = other.rows;
+        cols = other.cols;
+        matrix = new double*[rows];
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = new double[cols];
+            for (int j = 0; j < cols; j++)
+                matrix[i][j] = other.matrix[i][j];
+        }
+        error = other.error;
+
+        return *this;
+    }
+
+    // деструктор
+    ~Matrix() {
+        for (int i = 0; i < rows; i++)
+            delete[] matrix[i];
+        delete[] matrix;
+        count--;
+    }
+
+    // функція, яка присвоює елементу масиву деяке значення
+    void set(int i, int j, double value = 0.0) {
+        if (i >= rows || j >= cols) {
+            error = 1;
+            return;
+        }
+        matrix[i][j] = value;
+    }
+
+    // функція яка одержує деякий елемент матриці за індексами i та j
+    double get(int i, int j) const {
+        if (i >= rows || j >= cols) {
+            
+            return 0.0; // якщо помилка, поверне 0.0
+        }
+        return matrix[i][j];
+    }
+
+    // функція друку
+    void print() const {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                cout << matrix[i][j] << " ";
+            cout << endl;
+        }
+    }
+
+    // функція додавання двох матриць
+    Matrix operator+(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols)
+            return Matrix(); // якщо розміри матриць різні, повертаємо порожню матрицю зі станом помилки
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result.matrix[i][j] = matrix[i][j] + other.matrix[i][j];
+        return result;
+    }
+
+    // функція віднімання двох матриць
+    Matrix operator-(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols)
+            return Matrix(); // якщо розміри матриць різні, повертаємо порожню матрицю зі станом помилки
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result.matrix[i][j] = matrix[i][j] - other.matrix[i][j];
+        return result;
+    }
+
+    // функція множення двох матриць
+    Matrix operator*(const Matrix& other) const {
+        if (cols != other.rows)
+            return Matrix(); // якщо кількість стовпців першої матриці не дорівнює кількості рядків другої, повертаємо порожню матрицю зі станом помилки
+        Matrix result(rows, other.cols);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < other.cols; j++)
+                for (int k = 0; k < cols; k++) {
+					result.matrix[i][j] += matrix[i][k] * other.matrix[k][j]; 
+					return result; 
+					}
 	}
-	~Vec2() {
-		count--;
-		cout << " state Vec: " << state;
-		cout << " Vec delete \n";
-	}
-	Vec2(const Vec2&);
-	Vec2 Add(Vec2& d);
-	Vec2 Sub(Vec2& d);
-	Vec2 Mul(float d);
-	Vec2 Div(float d);
-	void Input();   //  !!! Без первантаження операцій    
-	void Output();  //  !!! Без первантаження операцій
-	bool CompLessAll(Vec2& s);
+	// функція отримання кількості створених об'єктів
 	static int getCount() {
-		if (count <= 0) cout << " Немає об'єктів Vec2 ";
 		return count;
 	}
-	int getState() { return state; }
 
-}; //кінець класу
-
-	int Vec2::count = 0; 
-	Vec2::Vec2(float ix, float iy, float iz) {
-		x = ix; y = iy; z = iz;
-		state = OK;
-		count++;
-	};
-	Vec2::Vec2(const Vec2& s) {
-		//if (this == &s) return; //  // the expression is used in the old standard
-		x = s.x; y = s.y; z = s.z; state = OK;
-		count++;
-	};
-	// Vec2::Vec2(float* v) {}
-	void Vec2::Input() {
-		cout << " Input  x y z";
-		cin >> x >> y >> z;
+	// функція отримання стану помилки
+	int getError() const {
+		return error;
 	}
-	void Vec2::Output() {
-		cout << " x =" << x << " y = " << y << " z = " << z << " state  " << state << endl;
-	}
+};
+int Matrix::count = 0;
+void testMatrix() {
+    
+    Matrix m1; // test default constructor
+    cout << "Matrix 1:" << endl;
+    m1.print();
 
-	Vec2 Vec2::Add(Vec2& s) {
-		Vec2 tmp;
-		tmp.x = x + s.x;
-		tmp.y = y + s.y;
-		tmp.z = z + s.z;
-		return tmp;
-	}
+    Matrix m2(3); // test constructor with one parameter
+    cout << "Matrix 2:" << endl;
+    m2.print();
 
-	Vec2 Vec2::Sub(Vec2& s) {
-		Vec2 tmp;
-		tmp.x = x - s.x;
-		tmp.y = y - s.y;
-		tmp.z = z - s.z;
-		return tmp;
-	}
-	Vec2 Vec2::Div(float d) {
-		Vec2 tmp;
-		if (fabs(d) < 1.e-25) {
-			tmp.state = BAD_DIV;
-			cout << " Error div \n";
-			return *this;
-		}
-		tmp.x = x / d;
-		tmp.y = y / d;
-		tmp.z = z / d;
-		return tmp;
-	}
-	Vec2 Vec2::Mul(float d) {
-		Vec2 tmp(*this);
-		tmp.x = x * d;
-		tmp.y = y * d;
-		tmp.z = z * d;
-		return tmp;
-	}
+    Matrix m3(2, 3, 1.5); // test constructor with three parameters
+    cout << "Matrix 3:" << endl;
+    m3.print();
 
-	bool Vec2::CompLessAll(Vec2& s) {
+    m1.set(0, 0, 2.5); // test set function
+    m1.set(1, 1, 3.5);
+    cout << "Matrix 1 after set function:" << endl;
+    m1.print();
 
-		if (x < s.x && y < s.y && z < s.z) return true;
-		return false;
-	}
+    double value = m3.get(1, 2); // test get function
+    cout << "Value at (1, 2) in Matrix 3: " << value << endl;
 
-	//Teстування
-	void mainExample3()
-	{
-		setlocale(LC_CTYPE, "ukr");
-		cout << "Тестування створенного класу \n";
-		cout << "Тестування конструкторiв \n";
-		cout << "Testing create class  \n";
-		cout << "Testing crot's  \n";
-		Vec2 ObjCDef;
-		ObjCDef.Output();
-		Vec2 ObjP1(10.0);
-		ObjP1.Output();
-		float  a = 1.0, b = 2.0;
-		Vec2  ObjP2(a, b);
-		ObjP2.Output();
-		Vec2 ObjCopy(ObjP2);
-		float* v = nullptr, v2[] = { 1.2f, 3.3f };
-		Vec2  ObjP3(v2);
-		if (ObjP3.getState() != OK) cout << " ObjP3  x= 0  y= 0  \n";
-		Vec2  ObjP4(v2);
-		if (ObjP4.getState() != OK) cout << " ObjP4 x= 0  y= 0  \n";
-		cout << " Кiлькiсть створених об'єктiв Vec2 " << Vec2::getCount() << endl;
-		cout << "Тестування введення \n";
-		ObjCDef.Input();
-		cout << "Тестування функцiй \n";
-		ObjCDef = ObjCDef.Add(ObjP2);
-		ObjCDef.Output();
-		cout << " \n Кiлькiсть створених об'єктiв Vec2 до Sub " << Vec2::getCount() << endl;
-		ObjCDef = ObjCDef.Sub(ObjP2);
-		cout << " \n Кiлькiсть створених об'єктiв Vec2 пiсля Sub " << Vec2::getCount() << endl;
+    Matrix m4 = m2; // test copy constructor
+    cout << "Matrix 4 (copy of Matrix 2):" << endl;
+    m4.print();
 
-		cout << "Testing input \n";
-		ObjCDef.Input();
-		cout << "Testing gunction \n";
-		ObjCDef = ObjCDef.Add(ObjP2);
-		ObjCDef.Output();
-		cout << " \n Counts create objects Vec2 before  Sub " << Vec2::getCount() << endl;
-		ObjCDef = ObjCDef.Sub(ObjP2);
-		cout << " \n  Counts create objects Vec2 after Sub  " << Vec2::getCount() << endl;
+    Matrix m5;
+    m5 = m3; // test assignment operator
+    cout << "Matrix 5 (assigned from Matrix 3):" << endl;
+    m5.print();
 
+    Matrix m6 = m1 + m5; // test + operator
+    cout << "Matrix 6 (result of Matrix 1 + Matrix 5):" << endl;
+    m6.print();
 
-		ObjCDef.Output();
-		ObjCDef = ObjCDef.Mul(5);
-		ObjCDef.Output();
-		ObjCDef = ObjCDef.Div(1.3);
-		if (ObjCDef.getState() == BAD_DIV) cout << "BAD_DIV \n";
-		ObjCDef.Output(); //0
+    Matrix m7 = m3 - m5; // test - operator
+    cout << "Matrix 7 (result of Matrix 3 - Matrix 5):" << endl;
+    m7.print();
 
-		ObjCDef = ObjCDef.Div(0.0);
-		if (ObjCDef.getState() == BAD_DIV) cout << "BAD_DIV \n";
-		ObjCDef.Output();
-		cout << "ObjCopy state " << ObjCopy.getState() << endl;
-		if (ObjCopy.CompLessAll(ObjCDef))  cout << "ObjCopy less ObjDef  " << endl;
+    Matrix m8 = m2 * m3; // test * operator
+    cout << "Matrix 8 (result of Matrix 2 * Matrix 3):" << endl;
+    m8.print();
 
+    int count = Matrix::getCount(); // test static function
+    cout << "Number of Matrix objects created: " << count << endl;
 
-		cout << "Завершення  тестування  \n";
+    int error = m7.getError(); // test getError function
+    cout << "Error state of Matrix 7: " << error << endl;
 
-		cout << "Completion of testing  \n";
-		cin.get();
-		return;
-	}
-
+    // test invalid parameter for set and get functions
+    m1.set(2, 0, 1.0);
+    value = m3.get(1, 3);
+    error = m3.getError();
+    cout << "After setting (2, 0) in Matrix 1 and getting (1, 3) in Matrix 3:" << endl;
+    cout << "Matrix 1:" << endl;
+    m1.print();
+    cout << "Matrix 3:" << endl;
+    m3.print();
+    cout << "Value at (1, 3) in Matrix 3: " << value << endl;
+    cout << "Error state of Matrix 3: " << error << endl;
+}
 
 int main()
 {
-	//  Код виконання завдань
-	//  Головне меню завдань
-	char ch = '5';
-	do {
-		system("cls");
-		std::cout << " Lab #3 \n";
-		cout << "Main menu: \n1)Task 1 \n2)Task 2 \n3) Exit\n\n";
-		cout << "Choose the task: ";
-		ch = cin.get();
-
-		cin.get();
-		
-		switch (ch) {
-		case '1': //task1();   
-			break;
-		case '2': mainExample3();   break;
-		case '3': // task2();   break;
-		case '4': return 0;
-		}
-		cout << "\nPress any key and enter\n";
-		ch = cin.get();
-	} while (ch != '3');
-	return 0;
-	//  Функції та класи можуть знаходитись в інших файлах проекту
+	cout << " Lab #3  !\n Additional Task: Press ENTER to start TEST: ";
+    cin.get();
+	cout << "Test started: \n";
+    
+	testMatrix();
+	cout << "\nTest Completed. \n";
 }
 
